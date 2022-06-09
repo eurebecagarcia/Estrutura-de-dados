e675aded7e23151d0f4c2c4c293c6b1eff5ffd8e
eu.rebecagarcia <rebecagarciarodrigues21@gmail.com>
2022-06-09 16:17:07 -0300

     my first script

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome ='Rebeca';
+let numero = 1;
+let anoAtual = 2022;
+
+
+console.log(anoAtual - numero)
+console.log(anoAtual + numero)
+console.log('Estou mexendo no console.log')
+
+let quartaFeira = false;
+console.log(typeof quartaFeira)
